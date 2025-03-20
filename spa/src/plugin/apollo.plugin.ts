b6759c93fb5ef309c38792cbe09eaade81c87173
nik-kita <nik@kita.github>
2025-03-20 21:01:05 +0100

    lab: eval on deno deploy

--- spa/src/plugin/apollo.plugin.ts
+++ spa/src/plugin/apollo.plugin.ts
@@ -6,7 +6,9 @@ import {
 export { DefaultApolloClient } from "@vue/apollo-composable";
 
 const httpLink = createHttpLink({
-  uri: "http://localhost:3000/graphql",
+  uri: import.meta.env.MODE === "production"
+    ? "https://fstorage.deno.dev/graphql"
+    : "http://localhost:3000/graphql",
 });
 const cache = new InMemoryCache({});
 
