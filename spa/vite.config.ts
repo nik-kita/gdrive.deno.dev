125d5970e3f3ace150e2ca9200bbdc95fd92d2a0
nik-kita <nik@kita.github>
2025-03-20 22:11:12 +0100

    lab3

--- spa/vite.config.ts
+++ spa/vite.config.ts
@@ -1,5 +1,4 @@
 import { fileURLToPath, URL } from "node:url";
-import { process } from "node:process";
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import vueDevTools from "vite-plugin-vue-devtools";
